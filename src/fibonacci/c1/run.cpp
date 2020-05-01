67015f7853c3d30a6cea43e0e5a5d1b56c84ac5f
gkonto <geokonto92@gmail.com>
2020-05-01 11:38:29 +0300

    Argparse is succesfully identified functions

--- src/fibonacci/c1/run.cpp
+++ src/fibonacci/c1/run.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "fib/fib.hpp"
+#include "../../argparser/argparser.hpp"
 
 
-int main(void) {
+int main(int argc, char **argv) {
+    ArgParser parser("Sequential fibonacci execution", argc, argv);
     std::cout << fib(33) << std::endl;
 }
