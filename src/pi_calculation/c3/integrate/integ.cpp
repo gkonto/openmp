5f60816191579248f10db39442312b276f1a8280
gkonto <geokonto92@gmail.com>
2020-05-07 18:59:17 +0300

    Added c3 variation

---
+++ src/pi_calculation/c3/integrate/integ.cpp
@@ -0,0 +1,36 @@
+#include <omp.h>
+#include "integ.hpp"
+
+
+//Example at p71
+static long num_steps = 1000000000;
+#define NUM_THREADS 2
+
+double pi() {
+    int nthreads = 0;
+    double pi = .0;
+    double step= 1.0/(double)num_steps;
+    omp_set_num_threads(NUM_THREADS);
+
+
+#pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        int nthrds = omp_get_num_threads();
+        double sum = 0.0;
+
+        if (id == 0) {
+            nthreads = nthrds;
+        }
+
+        for (int i = id, sum = 0.0; i < num_steps; i += nthreads) {
+            double x = (i + 0.5)*step;
+            sum += 4.0/(1.0 + x*x);
+        }
+#pragma omp critical
+        pi += sum * step;
+    }
+
+
+    return pi;
+}
