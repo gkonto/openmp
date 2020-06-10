98deac221d8a1d7a933b2ff7fe48ed1acbd352c4
gkonto <geokonto92@gmail.com>
2020-06-10 11:14:11 +0300

    Minor refactoring

--- src/prime_numbers/sequential/prime.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef PRIME_HPP
-#define PRIME_HPP
-double cpu_time ( );
-int prime_number ( int n );
-void timestamp ( );
-#endif
-
--- src/prime_numbers/sequential/run.cpp
+++ src/prime_numbers/sequential/run.cpp
@@ -5,32 +5,13 @@
 #include <omp.h>
 #include <stdlib.h>
 
-/*
-  Purpose:
-
-   PRIME_NUMBER_SWEEP does repeated calls to PRIME_NUMBER.
-  Parameters:
-    Input, int N_HI, the last value of N.
-    each iteration.
-*/
-static void prime_number_sweep(int n_hi) {
-  printf("\n");
-  printf("TEST01\n");
-  printf("  Call PRIME_NUMBER to count the primes from 1 to N.\n");
-  printf("\n");
-  printf("         N        Pi          Time\n");
-  printf("\n");
-
-  int primes = prime_number(n_hi);
-
-  printf("  %8d  %8d  %14f\n", n_hi, primes, ctime);
-}
 
 namespace {
-struct Opts {
-  int max = 0;
-};
+    struct Opts {
+      int max = 0;
+    };
 } // namespace
+
 
 void parseArgs(int argc, char **argv, Opts &o) {
   if (argc != 2) {
@@ -39,33 +20,62 @@ void parseArgs(int argc, char **argv, Opts &o) {
   }
   read_value<int>(argv[1], o.max);
 }
-/******************************************************************************/
+
+
+static void report_header() {
+    printf("\n");
+    printf("  Call PRIME_NUMBER to count the primes from 1 to N.\n");
+    printf("\n");
+    printf("         N        Pi          Time\n");
+    printf("\n");
+}
+
+
 /*
-  Purpose:
-    MAIN is the main program for prime_TEST.
+   PRIME_NUMBER_SWEEP does repeated calls to PRIME_NUMBER.
+  Parameters:
+    Input, int N_HI, the last value of N.
+    each iteration.
 */
+static void prime_number_sweep(int n_hi) {
+    report_header();
+    double start = omp_get_wtime();
+    int primes = prime_number(n_hi);
+    double end = omp_get_wtime();
+    printf("  %8d  %8d  %14f\n", n_hi, primes, end-start);
+}
+
+
+static void greetings() {
+    timestamp();
+    printf("\n");
+    printf("prime_TEST\n");
+    printf("  C version\n");
+    printf("  Test the prime library.\n");
+}
+
+
+static void bye() {
+    printf("\n");
+    printf("prime_TEST\n");
+    printf("  Normal end of execution.\n");
+    printf("\n");
+    timestamp();
+}
+
+
+// MAIN is the main program for prime_TEST.
 int main(int argc, char **argv) {
-  Opts o;
-  parseArgs(argc, argv, o);
-  timestamp();
-  printf("\n");
-  printf("prime_TEST\n");
-  printf("  C version\n");
-  printf("  Test the prime library.\n");
+    Opts o;
+    parseArgs(argc, argv, o);
+    greetings();
+    double start = omp_get_wtime();
+    prime_number_sweep(o.max);
 
-  double start = omp_get_wtime();
-  prime_number_sweep(o.max);
+    bye();
+    std::cout << "Execution Time: " << omp_get_wtime() - start << " seconds"<< std::endl;
 
-  // Terminate.
-  printf("\n");
-  printf("prime_TEST\n");
-  printf("  Normal end of execution.\n");
-  printf("\n");
-  timestamp();
-
-  std::cout << "Execution Time: " << omp_get_wtime() - start << " seconds"<< std::endl;
-
-  return 0;
+    return 0;
 }
 /******************************************************************************/
 
