fb5919ae77be1e685a4ba5268e16293f0281170e
gkonto <geokonto92@gmail.com>
2020-06-13 18:49:21 +0300

    Added a script to build all subprograms

--- src/build_all.py
+++ src/build_all.py
@@ -8,23 +8,34 @@ from os import makedirs, chdir
 examples = {
         "fibonacci" :["sequential", "c2"],
         "matrix_multiplication" : ["sequential", "c1", "c2"],
-        "pi_calculation" : ["sequential", "c1", "c2", "c3", "c4", "c5", "c6"]
+        "pi_calculation" : ["sequential", "c1", "c2", "c3", "c4", "c5", "c6"],
+        "jacobi" : ["sequential", "c1"],
+        "prime_numbers" : ["sequential", "c1"],
+        "pi_calculation" : ["sequential", "c1", "c2", "c3", "c4", "c5", "c6"],
+        "fft" : ["sequential", "c1"],
+        "heat_plate" : ["sequential", "c1"],
+        "fibonacci" : ["sequential", "c2"],
+        "linked_list_traversal" : ["sequential", "c1", "c2", "c3"],
+        "matrix_multiplication" : ["sequential", "c1", "c2"],
+        "mandelbrot" : ["sequential", "c1"],
+        "molecular_dynamics" : ["sequential", "c1"],
+        "mergesort" : ["sequential", "c1", "c2"],
+        "vectorization" : ["sequential", "c1"]
         }
 
-this_path = dirname(abspath(__file__))
-
-variations_paths = [join(this_path, ex, variation) for ex in examples for variation in examples[ex]]
-
-for var in variations_paths:
-    build_dir = join(var, "build")
-    if isdir(build_dir):
-        #TODO check output
-        subprocess.check_output("make -C {}".format(build_dir), shell = True)
-    else:
-        makedirs(build_dir)
-        chdir(build_dir)
-        subprocess.check_output("cmake ../", shell = True)
-        subprocess.check_output("make", shell = True)
+if __name__=="__main__":
+    this_path = dirname(abspath(__file__))
+    variations_paths = [join(this_path, ex, variation) for ex in examples for variation in examples[ex]]
+    for var in variations_paths:
+        build_dir = join(var, "build")
+        if isdir(build_dir):
+            #TODO check output
+            subprocess.check_output("make -C {}".format(build_dir), shell = True)
+        else:
+            makedirs(build_dir)
+            chdir(build_dir)
+            subprocess.check_output("cmake ../", shell = True)
+            subprocess.check_output("make", shell = True)
 
 
 
