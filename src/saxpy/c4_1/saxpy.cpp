c950377149a31ecde7dec80a0ce717d60394d2e0
gkonto <geokonto92@gmail.com>
2020-10-06 22:31:31 +0300

    c4_1

--- src/saxpy/c4_1/saxpy.cpp
+++ src/saxpy/c4_1/saxpy.cpp
@@ -4,7 +4,7 @@
 //TODO compile with -O0 -O2 and std==c++14
 
 void saxpy(size_t n, float a, const float *x, float *y) {
-    #pragma omp target map(tofrom: y[0:n]) map(to: x[0:n])
+    #pragma omp target
     for (size_t i = 0; i < n; ++i) {
         y[i] = a * x[i] + y[i]; 
     }
