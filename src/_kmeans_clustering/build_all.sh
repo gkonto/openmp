942708a4e0594651bc4cd0e364be0d50fbed35ab
gkonto <geokonto92@gmail.com>
2020-11-15 13:42:26 +0200

    :q

---
+++ src/_kmeans_clustering/build_all.sh
@@ -0,0 +1,2 @@
+g++ run1.cpp ../auxiliaries/auxiliaries.cpp -O0 -fopenmp -Wall -Wextra -I ../auxiliaries  -o ./builds/serial
+g++ run1.cpp ../auxiliaries/auxiliaries.cpp -O2 -fopenmp -Wall -Wextra -I ../auxiliaries  -o ./builds/serial_O2
