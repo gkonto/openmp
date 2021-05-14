d9b72ce17d9c41497219095b6b679e63ca10fbfa
Georgios Kontogiannis <geokonto92@gmail.com>
2021-05-14 19:34:32 +0300

    Delete src/_kmeans_clustering directory

--- src/_kmeans_clustering/build_all.sh
+++
@@ -1,2 +0,0 @@
-g++ run.cpp ../auxiliaries/auxiliaries.cpp -O0 -fopenmp -Wall -Wextra -I ../auxiliaries  -o ./builds/serial
-#g++ run.cpp ../auxiliaries/auxiliaries.cpp -O2 -fopenmp -Wall -Wextra -I ../auxiliaries  -o ./builds/serial_O2
